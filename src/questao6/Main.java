c87c99e52328eb10052aef3bbd3f41f74a78d01e
Mateus Alcantara <mateus.e.alcantara@outlook.com>
2023-04-20 08:01:18 -0300

    enxugando questão 6

--- src/questao6/Main.java
+++ src/questao6/Main.java
@@ -9,19 +9,12 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Digite um número (não muito grande, de preferência): ");
         int num = scanner.nextInt();
-        int digitos = numeroAlgarismos(num, 0);
         FilaDupla filaDupla = new FilaDupla(numeroAlgarismos(num, 0));
         filaDupla.insere(0); // naturalmente
         for (int i = 0; i < num; i++) {
             filaDupla = soma1(filaDupla);
             System.out.printf("%d: %s\n", i + 1, filaDupla); // uso do toString
         }
-        /*
-        FilaDupla resultado = new FilaDupla(filaDupla.getTamanho()); // apenas para inicializar
-        for (int i = 0; i < num; i++) {
-            resultado = soma1(filaDupla, resultado, 1);
-            System.out.printf("%d: %s\n", i, resultado); // uso do toString
-        }*/
 
     }
 
@@ -32,7 +25,7 @@ public class Main {
         return numeroAlgarismos(num / 2, ++contador);
     }
 
-    // tentando com recursividade
+    // tentando com recursividade, ainda não funciona
     public static FilaDupla soma1(FilaDupla filaDupla, FilaDupla resultado, int carry) {
         if (filaDupla.vazia()) {
             if (carry == 1)
